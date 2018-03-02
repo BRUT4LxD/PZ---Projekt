d4ec4d235e248b7c05c54087bb515ead32706f82
BRUT4LxD <tarsala95@gmail.com>
2018-03-03 00:20:15 +0100

    Nowy commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //Queries.printDatabase();
         new Windows.LogInWindow();
         System.out.println("This is a new line of code");
+        System.out.println("The next line added");
         //new Table();
 
     }
