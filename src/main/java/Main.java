521c862462c250c644700121a72092fd5ce47328
BRUT4LxD <tarsala95@gmail.com>
2018-03-03 00:27:29 +0100

    NowyBranchCommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,10 @@ public class Main {
         new Init();
         //Queries.printDatabase();
         new Windows.LogInWindow();
-        System.out.println("This is a new line of code");
-        System.out.println("The next line added");
+        new Windows.LogInWindow();
+        new Windows.LogInWindow();
+        new Windows.LogInWindow();
+        new Windows.LogInWindow();
         //new Table();
 
     }
